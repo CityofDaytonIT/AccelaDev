7a21228f11781932ade6a43efaf92fd2a48bb585
Christopher <cisensano@yahoo.com>
2017-09-22 11:45:30 -0700

    removed testing user

--- Scripts/Event/IRSA;Building!Complaint!~!~.js
+++ Scripts/Event/IRSA;Building!Complaint!~!~.js
@@ -1,7 +1,6 @@
 //Script 21
-if (inspResult == "Refer to Housing") {
-	currentUserId = "ADMIN"
-	childId = createChild("Enforcement","Case","NA","NA",capName )
+if(inspResult == "Refer to Housing"){
+	childId = createChild("Enforcement","Case","NA","NA",capName);
 
 	copyAppSpecific(capId, childId)
 	copyOwner(capId, childId)	
